5e006782a7e8562321c5aaac46531b437a25c586
YoGurt <islamdautov59@gmail.com>
2024-10-01 05:14:51 +0600

    Add project files.

---
+++ Practice1.Tests/CircleTests.cs
@@ -0,0 +1,75 @@
+﻿using Practice1;
+
+namespace Practice1.Tests
+{
+    [TestClass]
+    public  class CircleTests
+    {
+        private Circle? _areaCalculator;
+
+        [TestMethod]
+        public void CalculateCircleArea()
+        {
+            _areaCalculator = new Circle(10);
+
+            double expectedResult = 314;
+            double result = _areaCalculator.CalculateArea();
+
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+
+    [TestClass]
+    public class TriangleTests
+    {
+        private Triangle? _areaCalculator;
+
+        [TestMethod]
+        public void CalculateTriangleArea()
+        {
+            _areaCalculator = new Triangle(3, 4, 5);
+
+            double expectedResult = 6;
+            double result = _areaCalculator.CalculateArea();
+
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void TrinagleType()
+        {
+            _areaCalculator = new Triangle(3, 5, 4);
+
+            bool expectedResult = true;
+            bool result = _areaCalculator.isRight();
+
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+
+    [TestClass]
+    public class ShapeTests
+    {
+        private ShapeAreaCalculator? _areaCalculator;
+
+        [TestMethod]
+        public void CalculateCircleArea()
+        {
+            _areaCalculator = new ShapeAreaCalculator();
+            double expectedResult = 314;
+            double result = _areaCalculator.Calculate(10);
+
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void CalculateTriangleArea()
+        {
+            _areaCalculator = new ShapeAreaCalculator();
+            double expectedResult = 6;
+            double result = _areaCalculator.Calculate(3, 4, 5);
+
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
---
+++ Practice1/AreaCalculator.cs
@@ -0,0 +1,66 @@
+﻿namespace Practice1
+{
+    public abstract class Shape
+    {
+        public const double PI = 3.14;
+        public abstract double CalculateArea();
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius) 
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentException("Впишите положительное значение");
+            }
+            Radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Triangle (double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Впишите положительные значения");
+            }
+            A = a;
+            B = b;
+            C = c;
+        }
+        public override double CalculateArea()
+        {
+            double semiPerimeter = (A + B + C) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - A) * (semiPerimeter - B) * (semiPerimeter - C));
+        }
+
+        public bool isRight()
+        {
+            bool res = (Math.Pow(A, 2) == Math.Pow(B, 2) + Math.Pow(C, 2)) || (Math.Pow(B, 2) == Math.Pow(A, 2) + Math.Pow(C, 2)) || (Math.Pow(C, 2) == Math.Pow(A, 2) + Math.Pow(B, 2));
+            return res;
+        }
+    }
+
+    public class ShapeAreaCalculator
+    {
+        public double Calculate(double radius)
+        {
+            return new Circle(radius).CalculateArea();
+        }
+        public double Calculate(double a, double b, double c)
+        {
+            return new Triangle(a, b, c).CalculateArea();
+        }
+    }
+}
